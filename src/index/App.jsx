fdb5ba9166a40064186471e93d6039541801af8f
kewin <404502753@qq.com>
2020-08-09 21:15:14 +0800

    始发站交互实现

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -1,4 +1,5 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useMemo } from 'react'
+import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import './App.css'
 
@@ -8,26 +9,59 @@ import HighSpeed from './HighSpeed.jsx'
 import Journey from './Journey.jsx'
 import Submit from './Submit.jsx'
 
+// actions返回的是对象。需要将其传入到dispatch中
+import { exchangeFromTo, showCitySelector } from './actions'
+
 function App(props) {
+  const { from, to, dispatch } = props
   // 由于onBack没有引用到任何可变的变量,所以第二个参数为空
   const onBack = useCallback(() => {
     window.history.back()
+  }, [])
+
+  // 依赖于dispatch exchangeFromTo => 但是这三个值都不会变,所以传空数组
+  // const doExchangeFromTo = useCallback(() => {
+  //   dispatch(exchangeFromTo())
+  // }, [])
+
+  // const doShowCitySelector = useCallback(() => {
+  //   dispatch(showCitySelector())
+  // }, [])
+
+  const cbs = useMemo(() => {
+    return bindActionCreators(
+      {
+        exchangeFromTo,
+        showCitySelector,
+      },
+      dispatch
+    )
   }, [])
   return (
     <div>
       <div className="header-wapper">
         <Header title="火车票" onBack={onBack}></Header>
       </div>
-      <DepartDate></DepartDate>
-      <HighSpeed></HighSpeed>
-      <Journey></Journey>
-      <Submit></Submit>
+      <form className="form">
+        {/* <Journey
+        from={from}
+        to={to}
+        exchangeFromTo={doExchangeFromTo}
+        showCitySelector={doShowCitySelector}
+      ></Journey> */}
+        <Journey from={from} to={to} {...cbs}></Journey>
+        <DepartDate></DepartDate>
+        <HighSpeed></HighSpeed>
+        <Submit></Submit>
+      </form>
     </div>
   )
 }
 
+// connect将数据返回到当前组件的props中
 export default connect(
   function mapStateToProps(state) {
+    // 获取所有的数据
     return state
   },
   function mapDispatchToProps(dispatch) {
