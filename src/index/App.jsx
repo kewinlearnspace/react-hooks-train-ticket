afea6336681eab5b79c2ccb64ae9ad18638dbf68
kewin <404502753@qq.com>
2020-08-11 21:54:58 +0800

    查看列车类型切换

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -1,18 +1,18 @@
-import React, { useCallback, useMemo } from "react";
-import { bindActionCreators } from "redux";
-import { connect } from "react-redux";
-import "./App.css";
+import React, { useCallback, useMemo } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import './App.css'
 
-import Header from "../common/Header.jsx";
-import DepartDate from "./DepartDate.jsx";
-import HighSpeed from "./HighSpeed.jsx";
-import Journey from "./Journey.jsx";
-import Submit from "./Submit.jsx";
+import Header from '../common/Header.jsx'
+import DepartDate from './DepartDate.jsx'
+import HighSpeed from './HighSpeed.jsx'
+import Journey from './Journey.jsx'
+import Submit from './Submit.jsx'
 
-import CitySelector from "../common/CitySelector.jsx";
-import DateSelector from "../common/DateSelector.jsx";
+import CitySelector from '../common/CitySelector.jsx'
+import DateSelector from '../common/DateSelector.jsx'
 
-import { h0 } from "../common/fp";
+import { h0 } from '../common/fp'
 
 // actions返回的是对象。需要将其传入到dispatch中
 import {
@@ -23,8 +23,9 @@ import {
   setSelectedCity,
   showDateSelector,
   hideDateSelector,
-  setDepartDate
-} from "./actions";
+  setDepartDate,
+  toggleHighSpeed,
+} from './actions'
 
 function App(props) {
   const {
@@ -35,12 +36,13 @@ function App(props) {
     isDateSelectorVisible,
     cityData,
     isLoadingCityData,
-    departDate
-  } = props;
+    departDate,
+    highSpeed,
+  } = props
   // 由于onBack没有引用到任何可变的变量,所以第二个参数为空
   const onBack = useCallback(() => {
-    window.history.back();
-  }, []);
+    window.history.back()
+  }, [])
 
   // 依赖于dispatch exchangeFromTo => 但是这三个值都不会变,所以传空数组
   // const doExchangeFromTo = useCallback(() => {
@@ -56,11 +58,11 @@ function App(props) {
     return bindActionCreators(
       {
         exchangeFromTo,
-        showCitySelector
+        showCitySelector,
       },
       dispatch
-    );
-  }, []);
+    )
+  }, [])
 
   // 城市
   const citySlectorCbs = useMemo(() => {
@@ -68,42 +70,51 @@ function App(props) {
       {
         onBack: hideCitySelector, // onBack执行hideCitySelector()
         fetchCityData,
-        onSelect: setSelectedCity
+        onSelect: setSelectedCity,
       },
       dispatch
-    );
-  }, []);
+    )
+  }, [])
 
   // 日期
   const departDateCbs = useMemo(() => {
     return bindActionCreators(
       {
-        onClick: showDateSelector
+        onClick: showDateSelector,
       },
       dispatch
-    );
-  }, []);
+    )
+  }, [])
 
   const dateSelector = useMemo(() => {
     return bindActionCreators(
       {
-        onBack: hideDateSelector
+        onBack: hideDateSelector,
       },
       dispatch
-    );
-  }, []);
+    )
+  }, [])
 
   //
-  const onSelectDate = useCallback(day => {
+  const onSelectDate = useCallback((day) => {
     if (!day) {
-      return;
+      return
     }
     if (day < h0()) {
-      return;
+      return
     }
-    dispatch(setDepartDate(day));
-    dispatch(hideDateSelector());
-  }, []);
+    dispatch(setDepartDate(day))
+    dispatch(hideDateSelector())
+  }, [])
+
+  const highSpeedCbs = useMemo(() => {
+    return bindActionCreators(
+      {
+        toggle: toggleHighSpeed,
+      },
+      dispatch
+    )
+  }, [])
   return (
     <div>
       <div className="header-wapper">
@@ -118,7 +129,7 @@ function App(props) {
       ></Journey> */}
         <Journey from={from} to={to} {...cbs}></Journey>
         <DepartDate time={departDate} {...departDateCbs}></DepartDate>
-        <HighSpeed></HighSpeed>
+        <HighSpeed highSpeed={highSpeed} {...highSpeedCbs}></HighSpeed>
         <Submit></Submit>
       </form>
       <CitySelector
@@ -133,16 +144,16 @@ function App(props) {
         onSelect={onSelectDate}
       ></DateSelector>
     </div>
-  );
+  )
 }
 
 // connect将数据返回到当前组件的props中
 export default connect(
   function mapStateToProps(state) {
     // 获取所有的数据
-    return state;
+    return state
   },
   function mapDispatchToProps(dispatch) {
-    return { dispatch };
+    return { dispatch }
   }
-)(App);
+)(App)
--- src/index/HighSpeed.jsx
+++ src/index/HighSpeed.jsx
@@ -1,6 +1,23 @@
 import React from 'react'
+import classnames from 'classnames'
+import Prototype from 'prop-types'
 import './HighSpeed.css'
 
 export default function HighSpeed(props) {
-  return <div></div>
+  const { highSpeed, toggle } = props
+  return (
+    <div className="high-speed">
+      <div className="high-speed-label">只看高铁/动车</div>
+      <div className="high-speed-switch" onClick={() => toggle()}>
+        <input type="hidden" name="highSpeed" value={highSpeed} />
+        <div className={classnames('high-speed-track', { checked: highSpeed })}>
+          <span className={classnames('high-speed-handle', { checked: highSpeed })}></span>
+        </div>
+      </div>
+    </div>
+  )
+}
+HighSpeed.prototype = {
+  highSpeed: Prototype.bool.isRequired,
+  toggle: Prototype.func.isRequired,
 }
