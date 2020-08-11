1301db9e7e00930ac7b0fd27f22c0d889d647a2f
kewin <404502753@qq.com>
2020-08-11 23:24:54 +0800

    查询页数据结构与模块设计

--- src/query/App.jsx
+++ src/query/App.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { connect } from 'react-redux'
+
 import './App.css'
 
+import Nav from '../common/Nav.jsx'
+import Bottom from './Bottom.jsx'
+import List from './List.jsx'
 import Header from '../common/Header.jsx'
 
 function App(props) {
+  const onBack = useCallback(() => {
+    window.history.back()
+  }, [])
   return (
     <div>
-      <Header></Header>
+      <Header title="查询火车票" onBack={onBack}></Header>
     </div>
   )
 }
 
 export default connect(
-  function mapStateToProps(state) {},
-  function mapDispatchToProps(dispatch) {}
+  function mapStateToProps(state) {
+    // 获取所有的数据
+    return state
+  },
+  function mapDispatchToProps(dispatch) {
+    return { dispatch }
+  }
 )(App)
--- src/query/store.js
+++ src/query/store.js
@@ -3,4 +3,33 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import reducers from './reducers'
 import thunk from 'redux-thunk'
 
-export default createStore(combineReducers(reducers), {}, applyMiddleware(thunk))
+import { h0 } from '../common/fp'
+import { ORDER_DEPART } from './constant.js'
+
+export default createStore(
+  combineReducers(reducers),
+  {
+    from: null,
+    to: null,
+    departDate: h0(Date.now()),
+    highSpeed: false,
+    trainList: [], // 列车列表
+    orderType: ORDER_DEPART, // 排序类型
+    onlyTickets: false, // 只看有票
+    ticketTypes: [], // 坐席类型- 所有备选项
+    checkdTicketTypes: {}, // 坐席类型 - 被选中坐席
+    trainTypes: [], // 车次类型 - 所有车次
+    checkedTrainTypes: {}, // 车次类型 - 选中的测试
+    departStations: [], // 出发车站
+    checkedDepartStations: {}, // 被选中的出发车站
+    arriveStations: [], // 到达车站-所有类型
+    checkedArriveStations: {}, // 被转中的到达车站
+    departTimeStart: 0, // 出发站开始时间
+    departTimeEnd: 24, // 出发站结束时间
+    arriveTimeStart: 0, // 终点站开始时间
+    arriveTimeEnd: 24, // 终点站结束时间
+    isFiltersVisible: false, // 筛选浮层是否显示
+    searchParsed: false, // 控制数据获取后的是否展示
+  },
+  applyMiddleware(thunk)
+)
